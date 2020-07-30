3bfcede100771a40bad83f333a996210416e7a66
Rushika193 <66965019+Rushika193@users.noreply.github.com>
2020-07-30 16:37:40 +0530

    Delete forms.py

--- Management/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from manage.models import Posts
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model=Posts
-        fields=('FullName','PhoneNumber','Address',)
