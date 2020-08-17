f40b1b96927f6db90ac3689658a4b303d46b49bd
Rushika193 <66965019+Rushika193@users.noreply.github.com>
2020-08-17 12:36:57 +0530

    Add files via upload

--- Chatbot/chat.py
+++ Chatbot/chat.py
@@ -1,18 +1,29 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+from chatterbot.response_selection import get_first_response
+from chatterbot.comparisons import levenshtein_distance
 
+def get_response(message):
 
-chatbot=ChatBot('Kiki')
-trainer=ListTrainer(chatbot)
+    chatbot = ChatBot('kiki',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+           {
+            "import_path": "chatterbot.logic.BestMatch",
+             'maximum_similarity_threshold': 0.90,
+             "default_response": 'I am sorry, but I do not understand.',
+            },
+             ],
+            trainer='chatterbot.trainers.ListTrainer')
 
-trainer.train(['Hi, how are you?',
-                'I am good,What about you?',
-                    'Good too',
-                    'What would you prefer to do in free time?',
-                    'listening music,dance,cooking and reading,'])
+    
 
+    while True:
+        if message.strip()!='bye':
+            reply=chatbot.get_response(message)
+            response=str(reply)
+            return(response)
 
-while True:
-    request=input('user: ')
-    response=chatbot.get_response(request)
-    print('Kiki: ',response)
+        if message.strip()=='bye':
+            return("bye")
+            break
